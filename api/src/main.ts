3a1cea46cb314a6e028f0392ac6ebfed53d28877
mohamed 3rafa <mohamed3rafa01@gmail.com>
2026-02-10 22:39:11 +0200

    chore: Update listen address for Koyeb

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,11 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: 'http://localhost:3000', // Allow only your frontend
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT || 3001, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
